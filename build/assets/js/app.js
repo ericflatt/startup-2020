7ea5ab668d3b8b79ec9d179b519e616938b4cbaf
Eric Flatt <eflatt18@gmail.com>
2019-10-04 22:46:37 -0400

    hosting and other fixes

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,1 @@
-window.sr=ScrollReveal();
+window.sr=ScrollReveal();var countDownDate=new Date("Feb 15, 2020 15:37:25").getTime(),x=setInterval(function(){var e=(new Date).getTime(),t=countDownDate-e,n=Math.floor(t/864e5),o=Math.floor(t%864e5/36e5),a=Math.floor(t%36e5/6e4),r=Math.floor(t%6e4/1e3);document.getElementById("countdown").innerHTML=n+"d "+o+"h "+a+"m "+r+"s ",t<0&&(clearInterval(x),document.getElementById("countdown").innerHTML="EXPIRED")},1e3);
