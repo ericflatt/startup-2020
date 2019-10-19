91fcb69b315b08b879602b8070fa2edbef81e903
Eric Flatt <eflatt18@gmail.com>
2019-10-18 20:22:35 -0400

    mobile

---
+++ source/js/mobile.js
@@ -0,0 +1,35 @@
+(function () {
+    
+    scrollConverter.deactivate();
+  
+  }());
+
+  //countdown timer
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 8, 2020 11:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="countdown"
+  document.getElementById("mobileCountdown").innerHTML = days + " days, " + hours + " hours, "
+  + minutes + " minutes, " + seconds + " seconds";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("mobileCountdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
