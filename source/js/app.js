f948a6b0e689ce38c819fd8b513c0f5f3c19a1c1
Eric Flatt <eflatt18@gmail.com>
2020-01-14 16:46:52 -0500

    a solid days work

--- source/js/app.js
+++ source/js/app.js
@@ -27,33 +27,24 @@ $(function() {
 
 
 
-// //countdown timer
-// // Set the date we're counting down to
-// var countDownDate = new Date("Feb 8, 2020 11:00:00").getTime();
+var countDownDate = new Date("Feb 7, 2020 12:00:00").getTime();
 
-// // Update the count down every 1 second
-// var x = setInterval(function() {
+var x = setInterval(function() {
 
-//   // Get today's date and time
-//   var now = new Date().getTime();
+  var now = new Date().getTime();
     
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
+  var distance = countDownDate - now;
     
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-//   // Output the result in an element with id="countdown"
-//   document.getElementById("countdown").innerHTML = days + " days, " + hours + " hours, "
-//   + minutes + " minutes, " + seconds + " seconds";
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
     
-//   // If the count down is over, write some text 
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("countdown").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
