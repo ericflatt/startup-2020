a3ebf4503481c368c7ef8ad0fc627053ec15f2d9
Eric Flatt <eflatt18@gmail.com>
2020-01-19 21:28:06 -0500

    good enough for now cause i hate this website

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+
 $(function() {
     $("#contactButton").click(function() {
       $(".contactFormContainer").addClass("active");
