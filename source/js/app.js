a66cbe77a9cf063211e3fa772d6caaa0a0ac93a8
Eric Flatt <eflatt18@gmail.com>
2020-01-24 08:30:51 -0500

    a little of this, little of that

--- source/js/app.js
+++ source/js/app.js
@@ -71,11 +71,24 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML = days + " days " + hours + " hours "
+  + minutes + " minutes " + seconds + " seconds ";
     
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+var myVideo=document.getElementById("navVideo");
+
+function Pause()
+{
+  myVideo.pause();
+}
+
+function Play()
+{
+  if (myVideo.paused)
+  myVideo.play();
+}
